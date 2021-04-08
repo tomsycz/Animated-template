20d3d1926109fec70e6c8094d80a9ff80c6b413d
Thomas Syczyk <tomsycz@gmail.com>
2021-04-08 11:57:32 +0100

    update number two

--- navbar.js
+++ navbar.js
@@ -2,3 +2,5 @@ function toggleNavbar(collapseID) {
   document.getElementById(collapseID).classList.toggle("hidden");
   document.getElementById(collapseID).classList.toggle("block");
 }
+
+alert("stop");
